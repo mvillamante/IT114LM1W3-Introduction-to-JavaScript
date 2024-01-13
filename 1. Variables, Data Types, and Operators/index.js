b10db11e7b6087d10decc5290b1e51d9bcf1419d
Michaella Villamante <98160468+mvillamante@users.noreply.github.com>
2024-01-13 02:33:21 +0000

    completed exercise 2

--- 1. Variables, Data Types, and Operators/index.js
+++ 1. Variables, Data Types, and Operators/index.js
@@ -3,44 +3,91 @@
     - MDN Docs: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Expressions_and_Operators
 */
 
-// TODO 1.1 Use the var keyword to define a variable. Log the variable to the console. Now try moving the variable declaration after you log the variable.
+// DONE Use the var keyword to define a variable. Log the variable to the console. Now try moving the variable declaration after you log the variable.
+// Your code here
+var x = 10;
+var y = 6;
+var prod = x * y;
+console.log(prod);
+
+
+// DONE Use the let keyword to define a variable.
 // Your code here
 
-// TODO 1.2 Use the let keyword to define a variable.
+let a = 10;
+console.log(a);
+
+// DONE Use the const keyword to define a variable. Try reassigning the value of the variable. What happens?
 // Your code here
 
-// TODO 1.3 Use the const keyword to define a variable. Try reassigning the value of the variable. What happens?
-// Your code here
+const fname = "Michaella";
+const lname = "Villamante";
+console.log(fname,lname);
+
 
 // Checkpoint 1.1 What is the difference between var, let, and const?
-// Answer: 
+// Answer:
+/*var - variables are function scoped, meaning they are accessible only within the function in which they are defined
+let - variables declared with let are exclusively accessible within the block they are defined in, whether it is a function, an if statement, or a loop. 
+const - once assigned a value, constants cannot be reassigned or modified*/
 
-// TODO 1.4 Try out the different arithmetic operators
+// DONE Try out the different arithmetic operators
 // See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Expressions_and_Operators#arithmetic_operators
 // Your code here
 
+let num1 = 4;
+let num2 = 2;
+let answer = num1 ** num2;
+console.log(answer);
+
+let ans = (100 + 500) * num1;
+console.log(ans);
+
+let n1 = 10;
+let n2 = 8;
+let ntotal = n1 % n2;
+console.log(ntotal);
+
 // Checkpoint 1.2 What operators did you use?
-// Answer: 
-// Your code here
+// Answer: exponentiation, addition, multiplication, modulus
 
 // TODO 1.5 Try out the different string operators
 // See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Expressions_and_Operators#string_operators
 // Your code here
+let name1 = "Juliana";
+let name2 = "Noela";
+let result = name1 + " " + name2;
+let alp = name1 < name2;
+console.log(alp);
+console.log(result);
+
 
 // Checkpoint 1.3 What operators did you use?
-// Answer: 
+// Answer: + to concatenate, less than
 
 // TODO 1.6 Try out the different logical operators
 // See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Expressions_and_Operators#logical_operators
 // Your code here
 
+let cat = true;
+let dog = false;
+
+let animal1 = cat && dog;
+let animal2 = cat || dog;
+console.log(animal1);
+console.log(animal2);
+
 // Checkpoint 1.4 What operators did you use?
-// Answer: 
+// Answer: && and ||
 
 
 // TODO 1.7 Try out the different relational operators
 // See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Expressions_and_Operators#relational_operators
-
+const color = ["red", "orange", "yellow", "green"];
+console.log("colors");
+console.log(1 in color);
+console.log(0 in color);
+console.log("green" in color);
 
 // Checkpoint 1.5 What does [] == false evaluate to? Why does that happen? Research why
 // Answer: 
--- 2. Control Structures/index.js
+++ 2. Control Structures/index.js
@@ -5,32 +5,68 @@
 
 const randomNumber = Math.floor((Math.random() * 100) + 1); // feel free to mock this value for testing
 
-// TODO 2.1 Display "more than fifty" if randomNumber is more than fify
+// DONE Display "more than fifty" if randomNumber is more than fify
+// Your code here
+console.log(randomNumber);
+if (randomNumber > 50) {
+    console.log("more than fifty")
+}
+
+// DONE Display whether the random number is odd or even
 // Your code here
 
-// TODO 2.2 Display whether the random number is odd or even
-// Your code here
+if (randomNumber % 2 == 0) {
+    console.log("even");
+}
+else {
+    console.log("odd");
+}
 
 // TODO 2.3 If the number if a multiple of 3, write "fizz".
 // if the number is a multiple of 5 display "buzz".
 // if the number is divisible by both 3 and 5, display "fizzbuzz". otherwise, display the number
 // Your code here
 
+if (randomNumber % 3 == 0) {
+    console.log("fizz");
+}
+else if (randomNumber % 5 == 0) {
+    console.log("buzz");
+}
+else if (randomNumber % 3 == 0 && randomNumber % 5 == 0) {
+    console.log("fizzbuzz");
+}
+else {
+    console.log(randomNumber);
+}
+
 let toDisplay = ""
-// TODO 2.4 Use the ternary conditional operator to set toDisplay to "Even" if randomNumber is even and "Odd" if randomNumber is odd
+// DONE Use the ternary conditional operator to set toDisplay to "Even" if randomNumber is even and "Odd" if randomNumber is odd
 // Your code here
-console.log("toDisplay", toDisplay);
+toDisplay = randomNumber % 2 == 0 ? "Even" : "Odd";
+
+console.log("toDisplay:", toDisplay);
 
 // Checkpoint 2.1 How do you use switch statements and when would you use them? Try researching the answer
-// Answer: 
+// Answer: use switch to select one of many blocks of code to be executed
 
 // TODO 2.5 Use a for loop to print the numbers 1 to N
 const n = 10
 // Your code here
+for (let i = 0; i < n; i++) {
+    console.log(i + 1);
+}
 
 const list = ["apple", "banana", "cherry", "date", "elderberry"];
-// TODO 2.6 Use a while loop to display all the values in the list
+// DONE Use a while loop to display all the values in the list
 // Your code here
+let i = 0;
+let text = "";
+while (i < list.length) {
+    text += list[i];
+    console.log(list[i]);
+    i++;
+}
 
 // Checkpoint 2.3 How would you simulate a do-while loop in JavaScript
 // Answer: 
@@ -38,11 +74,23 @@ const list = ["apple", "banana", "cherry", "date", "elderberry"];
 // TODO 2.7 Use a for of loop to display all the values in the list
 // Your code here
 
+for (let x of list) {
+    text += x;
+    console.log(x);
+}
+
 // TODO 2.8 Use a for in loop to display all the values in the list
 // Your code here
 
+for (let y in list) {
+    text += list[y];
+    console.log(y);
+}
+
 // TODO 2.9 Use the for each method of the list to display all its values 
 // Your code here
+
+list.forEach((element) => console.log(element));
 
 // Checkpoint 2.2 When should you use for of, for in, or .forEach loops? Try researching the answer
 // Answer: 
@@ -54,3 +102,11 @@ const denominator = Math.floor((Math.random() * 5)); // feel free to mock this v
 
 const quotient = numerator / denominator;
 
+try {
+    const quotient = numerator / denominator;
+    if (denominator == 0) {
+        throw Errow("Division by zero Error")
+    }
+} catch (error) {
+    console.log("ERROR");
+}
